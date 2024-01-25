0a206ded4807a640ff5496bfa9c28f3ff7b0e46c
Tictaker3042 <il.lebed2011.gmail.com>
2024-01-25 18:30:55 +0300

    68

--- src/KalkulRtor.kt
+++ src/KalkulRtor.kt
@@ -1,14 +1,38 @@
 fun main (){
-    var x = readLine()?.toDoubleOrNull()!!
-    var b = readLine()!!
-    var y = readLine()!!.toDouble()
-    when (b) {
-        "+" -> {var r = x + y
-            println("$x + $y = ${x+y}")
-        }
-        "-" ->  println("$x - $y = ${x-y}")
-        "*" ->  println("$x * $y = ${x*y}")
-        "/" ->  println("$x / $y = ${x/y}")
-        else -> println("Ошибка")
-    }
+    val num1 = readLine()?.toFloat()!!
+    val znak = readLine()!!
+    val num2 = readLine()!!.toInt()
+    println(calculate(num1, num2, znak))
 }
+fun calculate (first_number:Float, second_number:Int, pes_patron:String) :String {
+    var response: String = ""
+    when (pes_patron) {
+        "-" -> {
+            response = (first_number - second_number).toString()
+        }
+        "+" -> {
+            response = (first_number + second_number).toString()
+        }
+        "*" -> {
+            response = (first_number * second_number).toString()
+        }
+        "/" -> {
+            response = (first_number / second_number).toString()
+        }
+        else -> "Неправильно"
+    }
+    return response
+}
+//fun main(){
+//    var maxNumber =Int.MIN_VALUE
+//    var input: String
+//    do{
+//        input = readLine()?:""
+//        val number =  input.toIntOrNull()
+//        number?.plus(number)
+//        if (number != null && number > maxNumber)
+//            maxNumber = number
+//    }while(input !="стоп")
+//
+//    println("$maxNumber")
+//}
