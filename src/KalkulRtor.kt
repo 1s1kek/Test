65202a3867ef93760874f1a2c445f916c52a0f3f
Tictaker3042 <il.lebed2011.gmail.com>
2024-01-26 09:11:28 +0300

    Поисх больших чисел из всех

--- src/KalkulRtor.kt
+++ src/KalkulRtor.kt
@@ -1,38 +1,37 @@
-fun main (){
-    val num1 = readLine()?.toFloat()!!
-    val znak = readLine()!!
-    val num2 = readLine()!!.toInt()
-    println(calculate(num1, num2, znak))
-}
-fun calculate (first_number:Float, second_number:Int, pes_patron:String) :String {
-    var response: String = ""
-    when (pes_patron) {
-        "-" -> {
-            response = (first_number - second_number).toString()
-        }
-        "+" -> {
-            response = (first_number + second_number).toString()
-        }
-        "*" -> {
-            response = (first_number * second_number).toString()
-        }
-        "/" -> {
-            response = (first_number / second_number).toString()
-        }
-        else -> "Неправильно"
-    }
-    return response
-}
-//fun main(){
-//    var maxNumber =Int.MIN_VALUE
-//    var input: String
-//    do{
-//        input = readLine()?:""
-//        val number =  input.toIntOrNull()
-//        number?.plus(number)
-//        if (number != null && number > maxNumber)
-//            maxNumber = number
-//    }while(input !="стоп")
-//
-//    println("$maxNumber")
+//fun main (){
+//    val num1 = readLine()?.toFloat()!!
+//    val znak = readLine()!!
+//    val num2 = readLine()!!.toInt()
+//    println(calculate(num1, num2, znak))
 //}
+//fun calculate (first_number:Float, second_number:Int, pes_patron:String) :String {
+//    var response: String = ""
+//    when (pes_patron) {
+//        "-" -> {
+//            response = (first_number - second_number).toString()
+//        }
+//        "+" -> {
+//            response = (first_number + second_number).toString()
+//        }
+//        "*" -> {
+//            response = (first_number * second_number).toString()
+//        }
+//        "/" -> {
+//            response = (first_number / second_number).toString()
+//        }
+//        else -> "Неправильно"
+//    }
+//    return response
+//}
+fun main(){
+    var maxNumber =Int.MIN_VALUE
+    var input: String
+    do{
+        input = readLine()?:""
+        val number =  input.toIntOrNull()
+        if (number != null && number > maxNumber)
+            maxNumber = number
+    }while(input !="стоп")
+
+    println("$maxNumber")
+}
